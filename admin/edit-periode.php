52a4f2b017c0857580a0a97543a4503822e7dadd
yogiealfin <yogiealfin23@gmail.com>
2023-02-03 04:00:12 +0700

    Fix: various bug

--- admin/edit-periode.php
+++ admin/edit-periode.php
@@ -104,7 +104,6 @@ require_once('../template/header.php');
 								<div class="form-group col-md-12">
 									<label class="font-weight-bold" for="id_status">Status Pegawai Yang Dinilai</label>
 									<select name="id_status" id="id_status" class="form-control">
-										<option value="">--Pilih Status--</option>
 										<?php foreach ($getStatus as $key) : ?>
 											<option value="<?= $key['id_status'] ?>" selected><?= $key['status'] ?></option>
 										<?php endforeach; ?>
