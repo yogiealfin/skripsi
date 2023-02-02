52ff5201b6a3ce4a0dc59a509e1c56b8691a52c4
yogiealfin <yogiealfin23@gmail.com>
2023-02-03 03:42:42 +0700

    Fix: tombol kembali tambah periode

--- admin/tambah-periode.php
+++ admin/tambah-periode.php
@@ -46,7 +46,7 @@ require_once('../template/header.php');
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
 	<h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-users"></i> Data Periode Penilaian</h1>
 
-	<a href="list-lowongan.php" class="btn btn-secondary btn-icon-split"><span class="icon text-white-50"><i class="fas fa-arrow-left"></i></span>
+	<a href="list-periode-penilaian.php" class="btn btn-secondary btn-icon-split"><span class="icon text-white-50"><i class="fas fa-arrow-left"></i></span>
 		<span class="text">Kembali</span>
 	</a>
 </div>
